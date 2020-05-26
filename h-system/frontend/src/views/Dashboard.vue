84412bb8f58c44486de31faac1bda93188d0dc5d
Miah Mohd <miahmohd98@gmail.com>
2020-05-26 03:26:58 +0200

    Login Dashboard pages

---
+++ h-system/frontend/src/views/Dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="dashboard">
+    <Navbar/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue'
+export default {
+  name: 'Dashboard',
+  components:{
+    Navbar
+  }
+}
+</script>
