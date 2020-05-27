c7f7489b37a8b8b51054aad29b66a019c7d34eae
LeonardoLonghi <leonardo.longhi@mail.polimi.it>
2020-05-27 15:17:19 +0200

    update readme

--- h-system/store/script.sql
+++ h-system/store/script.sql
@@ -9,8 +9,9 @@ CREATE TABLE Facility (
 CREATE TABLE Operator (
 	username VARCHAR(20) NOT NULL,
 	password CHAR(64) NOT NULL,
-	healthcare_id VARCHAR(20) NOT NULL,
-	facility VARCHAR(20) NOT NULL,
+	healthcare_id VARCHAR(20) ,
+    role ENUM('admin','operator'),
+	facility VARCHAR(20) ,
 	PRIMARY KEY (username),
 	FOREIGN KEY (facility) REFERENCES Facility(code)
 );
@@ -19,6 +20,7 @@ CREATE TABLE Certificate (
 	token_user VARCHAR(20) NOT NULL,
 	t_id VARCHAR(40) NOT NULL,
 	operator VARCHAR(20) NOT NULL,
+	status ENUM('active','disabled')
 	PRIMARY KEY (token_user,t_id),
 	FOREIGN KEY (operator) REFERENCES Operator(username)
 );
@@ -29,9 +31,10 @@ INSERT INTO Facility (code,name, address,type) VALUES(
     "Indirizzo dell'ospedale",
     "hospital"
 );
-INSERT INTO Operator (username,password,healthcare_id,facility) VALUES(
+INSERT INTO Operator (username,password,healthcare_id,role,facility) VALUES(
     "Giovanni",
     "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8",
     "MEDICO0123",
+    "operator",
     "OSPEDALE123"
 );
