945a92bc779bb94dee0ff46a28ff337ec237d02a
Miah Mohd <miahmohd98@gmail.com>
2020-05-28 02:10:45 +0200

    Bugfix

--- h-system/api/routes/login.js
+++ h-system/api/routes/login.js
@@ -2,19 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/', (req, res) => {
-    
     const { body } = req;
     const { username } = body;
     const { password } = body;
 
-    const authenticationService = require('./service/Authentication');
+    const authenticationService = require('../service/Authentication');
     token = authenticationService.login(username, password, ((error, token) => {
-        if(error || token == null) res.status(400);
-        res.sendJson({
+        if (token == null)
+            res.status(401);
+
+        res.json({
             error,
             token
         })
-        
     }));
 });
 
