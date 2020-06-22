e18c3f799b6828a6a44e645eb56e4e248059f2a1
Miah Mohd <mohdehtesham.miah@mail.polimi.it>
2020-06-22 23:31:58 +0200

    Add navbar in the login page

--- h-system/frontend/src/views/Login.vue
+++ h-system/frontend/src/views/Login.vue
@@ -1,19 +1,25 @@
 <template>
-  <div class="container d-flex align-items-center justify-content-center">
-    <form @submit.prevent="login">
-      <div class="form-group">
-        <label for="exampleInputEmail1">Username</label>
-        <input v-model="username" type="text" class="form-control" />
-      </div>
-      <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input v-model="password" type="password" class="form-control" />
-      </div>
-      <div v-if="!isSuccess" class="form-group">
-        <small class="form-text text-danger">Incorrect credentials</small>
-      </div>
-      <button type="submit" class="btn btn-primary">Login</button>
-    </form>
+  <div class="login d-flex flex-column">
+    <nav class="navbar navbar-light bg-light">
+      <span class="navbar-brand">Healthcare</span>
+    </nav>
+
+    <div class="container flex-grow-1 d-flex align-items-center justify-content-center">
+      <form @submit.prevent="login">
+        <div class="form-group">
+          <label for="exampleInputEmail1">Username</label>
+          <input v-model="username" type="text" class="form-control" />
+        </div>
+        <div class="form-group">
+          <label for="exampleInputPassword1">Password</label>
+          <input v-model="password" type="password" class="form-control" />
+        </div>
+        <div v-if="!isSuccess" class="form-group">
+          <small class="form-text text-danger">Incorrect credentials</small>
+        </div>
+        <button type="submit" class="btn btn-primary">Login</button>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -45,7 +51,7 @@ export default Vue.extend({
 </script>
 
 <style scoped>
-.container {
+.login {
   height: 100%;
 }
 </style>
--- p-system/frontend/src/components/Navbar.vue
+++ p-system/frontend/src/components/Navbar.vue
@@ -1,14 +1,14 @@
 <template>
   <nav class="navbar navbar-light bg-light">
-    <router-link :to="{ name: home}" class="navbar-brand">P-System</router-link>
+    <router-link :to="{ name: home}" class="navbar-brand">Civil protection</router-link>
     <!-- <a @click="$router.push({name:home})" class="navbar-brand">P-System</a> -->
     <!-- Mettere nome operatore loggato -->
     <div class="d-flex">
-      <router-link :to="{name: btnNewRoute}"  class="btn btn-primary mr-3">{{btnNewString}}</router-link>
+      <router-link :to="{name: btnNewRoute}" class="btn btn-primary mr-3">{{btnNewString}}</router-link>
       <!-- <button
         @click="$router.push({name: btnNewRoute})"
         class="btn btn-primary mr-3"
-      >{{btnNewString}}</button> -->
+      >{{btnNewString}}</button>-->
       <button @click="logout" class="btn btn-outline-secondary">Logout</button>
     </div>
   </nav>
